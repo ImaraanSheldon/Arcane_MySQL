62335be437fba341c4b532c89f5b582de60dfb87
ImaraanSheldon <imaraansheldonis2302003@gmail.com>
2024-08-10 23:17:24 +0200

    hello

---
+++ Users_&_Admin.sql
@@ -0,0 +1,12 @@
+create table users(
+	userName varchar(45),
+    userSurname varchar(45),
+    userEmail varchar(45),
+    safeWord text
+);
+create table admin(
+	adminName varchar(45),
+    adminSurname varchar(45),
+    adminEmail varchar(45),
+    safeWord text
+);
